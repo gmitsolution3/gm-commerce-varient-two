44fe1743784c3f39afdff2520a34ffbeb593b4d9
arjsabbir88 <arjsabbir88@gmail.com>
2026-01-13 18:27:20 +0600

    almost done hatkhola e-commerce

--- app/components/skeleton/ServicesSkeleton.tsx
+++ app/components/skeleton/ServicesSkeleton.tsx
@@ -6,6 +6,9 @@ export function ServicesSkeleton() {
       <CardSkeleton />
       <CardSkeleton />
       <CardSkeleton />
+      <CardSkeleton />
+      <CardSkeleton />
+      <CardSkeleton />
     </div>
   );
 }
